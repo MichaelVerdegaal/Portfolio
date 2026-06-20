a79992dc31398e03c7c1306fc196169b787a2adb
Michael Verdegaal <michaelverdegaal@hotmail.com>
2026-06-20 20:31:39 +0200

    chore: format code

--- main.py
+++ main.py
@@ -7,7 +7,11 @@ with open('skills.yaml', 'r') as file:
         loaded_data = yaml.safe_load(file)
 app = FastAPI()
 
+# Mount Python files
 app.mount("/src", StaticFiles(directory="src"), name="src")
+# Mount static files (pyscript.json)
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
 
 @app.get("/")
 async def root():
--- src/canvas.py
+++ src/canvas.py
@@ -1,6 +1,18 @@
 from pyscript import when, display
+# import matplotlib.pyplot as plt
 
+# def create_graph():
+#     # sample data
+#     x = [1, 2, 3, 4, 5]
+#     y = [2, 3, 5, 7, 11]
+
+#     # plot
+#     fig, ax = plt.subplots()
+#     ax.scatter(x, y, vmin=0, vmax=100)
+#     ax.set(xlim=(0, 8), xticks=range(0, 9), ylim=(0, 8), yticks =range(0, 9))
+#     plt.show()
 
 @when("click", "#clickMe")
 def handler():
+    # create_graph()
     display("Button clicked!")
