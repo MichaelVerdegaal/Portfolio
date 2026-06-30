e1242c49741ccb2510ceaf464bcb53c539fefa14
Michael Verdegaal <michael.verdegaal@dfg.nl>
2026-06-30 17:21:24 +0200

    feat: new animation

--- test_animation.py
+++ test_animation.py
@@ -2,7 +2,7 @@ from matplotlib.patches import ConnectionPatch
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.axes import Axes
-from matplotlib.collections import PathCollection
+from matplotlib.collections import PathCollection, LineCollection
 from matplotlib.figure import Figure
 from matplotlib.path import Path
 from matplotlib.animation import FuncAnimation
@@ -27,50 +27,44 @@ YLIM_MIN, YLIM_MAX = 0, 100
 fig, ax = plt.subplots()
 ax.set(xlim=(XLIM_MIN, XLIM_MAX), ylim=(YLIM_MIN, YLIM_MAX))
 
+# Create indices
+node_index = {name: i for i , name in enumerate(NODES)}
+edge_index = np.array(
+    [(node_index[v], node_index[u]) for v, neighbors in GRAPH.items() for u in neighbors]
+)
+
 # Create coordinates
-nodes: np.ndarray = np.random.randint(XLIM_MIN + 10, XLIM_MAX - 10, size=(len(NODES), 2))
+nodes_coords = np.random.uniform(low=XLIM_MIN + 20, high=XLIM_MAX - 20, size=(len(NODES), 2))
 
 # Draw nodes
-scatter: PathCollection = ax.scatter(nodes[:, 0], nodes[:, 1])
+scatter: PathCollection = ax.scatter(nodes_coords[:, 0], nodes_coords[:, 1])
 
 # Draw edges
-# for edge in EDGES:
-#     node1, node2 = edge
-#     x1, y1 = node_dict[node1]
-#     x2, y2 = node_dict[node2]
-#     ax.add_patch(
-#         ConnectionPatch(
-#             xyA=(x1, y1),
-#             xyB=(x2, y2),
-#             coordsA=ax.transData,
-#             arrowstyle="-|>",
-#             color="black",
-#             linewidth=1,
-#         )
-#     )
+edge_lines = LineCollection(nodes_coords[edge_index], colors="black", linewidths=1, zorder=1)
+ax.add_collection(edge_lines)
+
     
+AMPLITUDE = 5
+phase = np.linspace(0, 2 * np.pi, len(NODES), endpoint=False).astype(np.float32)
 
 def animate(frame: int):
-    ANGLE = frame % 360
-    AMPLITUDE = 2
-
     # Get coordinates
     coordinates: np.ndarray = scatter.get_offsets()
 
     # Transform coordinates
-    coordinates = coordinates - AMPLITUDE * math.sin(ANGLE)
-
+    coordinates[:, 1] += AMPLITUDE * np.sin(frame + phase)
 
     # Set new coordinates
     scatter.set_offsets(coordinates)
+    edge_lines.set_segments(coordinates[edge_index])   
     return (scatter,)
 
 
 # get first object of the PathCollection
 first_path: Path = scatter.get_paths()[0]
 
-anim = FuncAnimation(fig, animate, interval=100, frames=60, repeat=True)
+anim = FuncAnimation(fig, animate, interval=200, frames=20, repeat=True)
 
 fig.tight_layout()
-# anim.save(filename="animation.gif", writer="pillow")
+anim.save(filename="animation.gif", writer="pillow")
 plt.show()
