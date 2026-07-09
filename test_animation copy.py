411d98665fd991a5a76ea918d8c0211a9c862458
Michael Verdegaal <michael.verdegaal@dfg.nl>
2026-07-09 10:20:31 +0200

    refactor: add more class properties

--- test_animation copy.py
+++ test_animation copy.py
@@ -1,3 +1,7 @@
+from numpy._typing._array_like import NDArray
+
+from numpy import float64
+
 from numpy._core import dtype
 import numpy.typing as npt
 from typing import Any
@@ -37,7 +41,7 @@ class Graph:
 
     def __init__(
         self,
-        graph: dict[str, list[str] | None],
+        graph: dict[str, list[str]],
         axis_lim: tuple[int, int] = (0, 100),
         spawn_margin: int = 20,
     ):
@@ -79,19 +83,66 @@ class Graph:
     def coords_y(self) -> npt.NDArray[np.float64]:
         return self.coords[:, 1]
 
+    @property
+    def edges_start(self) -> npt.NDArray[np.int32]:
+        return self.edges[:, 0]
+
+    @property
+    def edges_end(self) -> npt.NDArray[np.int32]:
+        return self.edges[:, 1]
+
+    @property
+    def edge_lengths(self) -> npt.NDArray[np.float64]:
+        d = self.coords[self.edges_end] - self.coords[self.edges_start]
+        return np.hypot(d[:, 0], d[:, 1])
+
 
 class GraphScene:
     def __init__(self, graph: Graph, fig: Figure, ax: Axes):
         self.graph: Graph = graph
 
         # Create nodes/edges with PathCollection and LineCollection
+        self._scatter: PathCollection = ax.scatter(
+            graph.coords_x, graph.coords_y, zorder=2
+        )
         self._edge_lines: LineCollection = LineCollection(
             [], colors="black", linewidths=1, zorder=1
         )
         _ = ax.add_collection(self._edge_lines)
-        self._scatter: PathCollection = ax.scatter(
-            graph.coords_x, graph.coords_y, zorder=2
-        )
+        self._edge_lines.set_segments(self.graph.coords[graph.edges])
+
+    @property
+    def coords(self) -> npt.NDArray[np.float64]:
+        return self.graph.coords
+
+    @coords.setter
+    def coords(self, new_coords: npt.NDArray[np.float64]) -> None:
+        self.graph.coords = new_coords
+        self.move_nodes(new_coords)
+
+    @property
+    def coords_x(self) -> npt.NDArray[np.float64]:
+        return self.graph.coords_x
+
+    @property
+    def coords_y(self) -> npt.NDArray[np.float64]:
+        return self.graph.coords_y
+
+    @property
+    def edges(self) -> npt.NDArray[np.int32]:
+        return self.graph.edges
+
+    @property
+    def edges_start(self) -> npt.NDArray[np.int32]:
+        return self.graph.edges_start
+
+    @property
+    def edges_end(self) -> npt.NDArray[np.int32]:
+        return self.graph.edges_end
+
+    @property
+    def edge_lengths(self) -> npt.NDArray[np.float64]:
+        return self.graph.edge_lengths
 
     def move_nodes(self, new_coords: npt.ArrayLike) -> None:
         self._scatter.set_offsets(new_coords)
@@ -103,3 +154,33 @@ fig, ax = create_figure()
 G: Graph = Graph(graph_dict)
 GScene: GraphScene = GraphScene(G, fig, ax)
 
+
+# TODO
+
+# target_len: NDArray[float64] = GScene.graph.edge_lengths
+# # history = step_history(
+# #     scene.coords,
+# #     lambda pos: equalize_edges_step(pos, scene.edges, target_len),
+# #     FRAMES,
+# # )
+# final_correction = equalize_edges_step(
+#     GScene.graph.coords, GScene.graph.edges, target_len, step_size=1.0
+# )
+# final_layout = GScene.graph.coords + final_correction
+# history = tween_history(GScene.graph.coords, final_layout, FRAMES)
+
+
+def animate(frame: int):
+    coords = GScene.coords
+
+    # transform
+    new_coords = coords + np.random.uniform(-1, 1, size=coords.shape) * 0.5
+
+    # update scene
+    GScene.coords = new_coords
+    return GScene._scatter, GScene._edge_lines
+
+
+anim = FuncAnimation(fig, animate, interval=INTERVAL_MS, frames=FRAMES, repeat=True)
+fig.tight_layout()
+plt.show()
